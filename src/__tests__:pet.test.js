0329546cf8dfda76b92c6ebf457b96c88a7ef25c
Jasmine Amber <jasmineamber.ja@gmail.com>
2019-06-22 13:14:38 +0100

    committing virtual pet - added grow up function

---
+++ src/__tests__:pet.test.js
@@ -0,0 +1,24 @@
+const Pet = require('../src/pet.js');
+
+
+describe('constructor', () => {
+    it('returns an object', () => {
+        expect(new Pet('Fido')).toBeInstanceOf(Object);
+    });
+    it('sets the name property', () => {
+        const pet = new Pet('Fido');
+        expect(pet.name).toEqual('Fido');
+    });
+    it('has an initial age of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.age).toEqual(0);
+    });
+});
+
+describe('growUp', () => {
+    it('increments the age by 1', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.age).toEqual(1);
+    });
+})
